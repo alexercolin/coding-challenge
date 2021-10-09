6a40fa1659d37631c809222d9c14f85cdf50b8e1
alexercolin <alex_ercolin@hotmail.com>
2021-10-09 15:31:24 -0300

    fix:identation

---
+++ Time-Conversion/index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+  const [hour, minute, second] = s.split(":");
+
+  let [hours, minutes] = hour.split(":");
+
+  if (hour === "12") {
+    hours = "00";
+  }
+
+  if ([second.substring(2, 4)] == "PM") {
+    hours = parseInt(hours, 10) + 12;
+  }
+
+  return `${hours}:${minute}:${second.substring(0, 2)}`;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  const result = timeConversion(s);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
